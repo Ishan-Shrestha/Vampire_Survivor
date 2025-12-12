f11f1e09f528efe4c59dd7733baa6b33925b750a
Ishan-Shrestha <171509510+Ishan-Shrestha@users.noreply.github.com>
2025-12-12 20:45:20 +0545

    Added resources and inital game setting

---
+++ code/main.py
@@ -0,0 +1,39 @@
+from settings import *
+from player import Player
+
+class Game:
+    def __init__(self):
+        # setup
+        pygame.init()
+        self.WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Vampire Survivor")
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        # sprites
+        self.player = Player((400,300), self.all_sprites)
+    
+    def run(self):
+        while self.running:
+            dt = self.clock.tick()/1000
+
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            # update
+            self.all_sprites.update(dt)
+
+            # draw
+            self.WINDOW.fill('black')
+            self.all_sprites.draw(self.WINDOW)
+            
+            pygame.display.update()
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
